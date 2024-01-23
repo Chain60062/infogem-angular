0dbcf65e35d4cf7b51888f54b495ad1ee6cfdba7
Chain60062 <viniciusmmenezes0@gmail.com>
2024-01-23 19:12:51 -0300

    Route improvements.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { CartComponent } from './pages/cart/cart.component';
 import { MainComponent } from './pages/main/main.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductComponent } from './pages/product/product.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'cart', component: CartComponent, title: 'Carrinho' },
+  { path: 'profile/:username', component: ProfileComponent },
+  { path: 'product/:id/:slug', component: ProductComponent },
+  { path: '**', component: NotfoundComponent, title: '404 - Não Encontrado' },
+  // { path: 'product/:id/', redirectTo: '/product/:id', pathMatch: 'full' },
 ];
